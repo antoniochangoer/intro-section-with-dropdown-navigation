aad04ed7b22b5d46ff2fa6b433c4dacb13fbbf73
antoniochangoer <54006126+antoniochangoer@users.noreply.github.com>
2022-11-02 11:01:22 +0100

    sidemenu done

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const openMenu = document.querySelector('.nav__hamburger');
 const closeMenu = document.querySelector('.close');
 const sideMenu = document.querySelector('.side-menu');
 const backDrop = document.querySelector('#backdrop');
+const featuresMenuToggle = document.querySelector('.features-toggle');
+const companyMenuToggle = document.querySelector('.company-toggle');
 
 openMenu.addEventListener('click', () => {
     sideMenu.classList.add('show-menu');
@@ -16,4 +18,14 @@ closeMenu.addEventListener('click', () => {
 backDrop.addEventListener('click', () => {
     sideMenu.classList.remove('show-menu');
     backDrop.classList.remove('backdrop');
+});
+
+featuresMenuToggle.addEventListener('click', (e) => {
+    const dropdownFeatures = document.querySelector('.features-dropdown__items');
+    dropdownFeatures.classList.toggle('dropdown-show');
+});
+
+companyMenuToggle.addEventListener('click', (e) => {
+    const dropdownCompany = document.querySelector('.company-dropdown__items');
+    dropdownCompany.classList.toggle('dropdown-show');
 });
